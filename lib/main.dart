5e6aa5daedeff38802835b8e47587fa2ab271314
Moe Hasegawa <moe.hasegawa@psp.co.jp>
2026-07-03 18:08:02 +0900

    課題1−1 背景色変更

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: .fromSeed(seedColor: Colors.pink),
+        scaffoldBackgroundColor:Colors.pink[100], 
+
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -76,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      // backgroundColor: Colors.pink,
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
