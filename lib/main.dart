9d335341e8a650ee80c0286bc7c5ac72b9ad62e6
Moe Hasegawa <moe.hasegawa@psp.co.jp>
2026-08-19 19:25:38 +0900

    課題４　それぞれのグループ名をタップすると、画面遷移してそのグループの詳細情報が表示される（アイコン未完成）

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-import 'package:flutter_tutorial/next_page.dart';
-import 'enum_gender.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_tutorial/enum_part.dart';
+import 'package:flutter_tutorial/group_model.dart';
+import 'package:flutter_tutorial/member_model.dart';
+import 'package:flutter_tutorial/next_page_details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,14 +30,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Group mrsGreenApple = Group(
+    groupName: "Mrs. GREEN APPLE",
+    image: "assets/images/Mrs.GREENAPPLE.jpeg",
+    formation: 2013,
+    members: [
+      Member(
+        memberName: "大森元貴",
+        memberAge: 29,
+        memberPart: [Part.vocal, Part.guitar],
+      ),
+      Member(memberName: "藤澤涼架", memberAge: 33, memberPart: [Part.keyboard]),
+      Member(memberName: "若井滉斗", memberAge: 29, memberPart: [Part.guitar]),
+    ],
+  );
+
+  Group kingGnu = Group(
+    groupName: "King Gnu",
+    image: "assets/images/KingGnu.jpeg",
+    formation: 2017,
+    members: [
+      Member(
+        memberName: "井口理",
+        memberAge: 32,
+        memberPart: [Part.vocal, Part.keyboard],
+      ),
+      Member(
+        memberName: "常田大希",
+        memberAge: 34,
+        memberPart: [Part.guitar, Part.vocal],
+      ),
+      Member(memberName: "新井和輝", memberAge: 33, memberPart: [Part.bassGuitar]),
+      Member(memberName: "勢喜遊", memberAge: 33, memberPart: [Part.drum]),
+    ],
+  );
+
+  Group officialHigedanDism = Group(
+    groupName: "Official髭男dism",
+    image: "assets/images/Official髭男dism.jpg",
+    formation: 2012,
+    members: [
+      Member(
+        memberName: "藤原聡",
+        memberAge: 34,
+        memberPart: [Part.vocal, Part.keyboard],
+      ),
+      Member(memberName: "小笹大輔", memberAge: 32, memberPart: [Part.guitar]),
+      Member(memberName: "楢﨑誠", memberAge: 37, memberPart: [Part.bassGuitar]),
+      Member(memberName: "松浦匡希", memberAge: 33, memberPart: [Part.drum]),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(
-          child: Text(widget.title, style: TextStyle(color: Colors.white)),
-        ),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
       body: Center(
         child: SizedBox(
@@ -60,37 +106,90 @@ class _MyHomePageState extends State<MyHomePage> {
                     padding: const EdgeInsets.all(35.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [Text("Band01"), Text("＞")],
+                      children: [
+                        Text(mrsGreenApple.groupName!),
+                        Icon(
+                          Icons.chevron_right,
+                          color: Colors.blueAccent,
+                          size: 33.0,
+                        ),
+                      ],
                     ),
                   ),
                 ),
                 onTap: () {
-                  Navigator.push(context, route);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          NextPageDetails(groupDetails: mrsGreenApple),
+                    ),
+                  );
                 },
               ),
 
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-                child: Padding(
-                  padding: const EdgeInsets.all(35.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text("Band02"), Text("＞")],
+              GestureDetector(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(35.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(kingGnu.groupName!),
+                        Icon(
+                          Icons.chevron_right,
+                          color: Colors.blueAccent,
+                          size: 33.0,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          NextPageDetails(groupDetails: kingGnu),
+                    ),
+                  );
+                },
               ),
 
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-                child: Padding(
-                  padding: const EdgeInsets.all(35.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text("Band03"), Text("＞")],
+              GestureDetector(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(35.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(officialHigedanDism.groupName!),
+                        Icon(
+                          Icons.chevron_right,
+                          color: Colors.blueAccent,
+                          size: 33.0,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          NextPageDetails(groupDetails: officialHigedanDism),
+                    ),
+                  );
+                },
               ),
             ],
           ),
